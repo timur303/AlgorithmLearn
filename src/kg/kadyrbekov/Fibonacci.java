0f213c195392bc49c210516fe71411f3edec74ed
Тимур <kadyrbekovtimur001@gmail.com>
2023-01-29 14:32:06 +0600

    commit-second

--- src/kg/kadyrbekov/Fibonacci.java
+++ src/kg/kadyrbekov/Fibonacci.java
@@ -1,5 +1,7 @@
 package kg.kadyrbekov;
 
+import java.util.Arrays;
+
 /**
  * @author Timur Kadyrbekov
  */
@@ -7,9 +9,15 @@ package kg.kadyrbekov;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println(fibEffective(100)  + " Effective ");
         System.out.println(fibNaive(5) + " Naive ");
+        System.out.println(fibEffective(100) + " Effective ");
 
+        int n = 100;
+        long [] mem = new long[n+1];
+
+        Arrays.fill(mem,-1);
+
+        System.out.println(fibNaiveMem(n,mem)+ " With Mem" );
     }
 
     //naive, slow, obvious
@@ -22,7 +30,7 @@ public class Fibonacci {
 
     //effective
     public static long fibEffective(int n) {
-        long[] arr = new long[n+1];
+        long[] arr = new long[n + 1];
 
         arr[0] = 0;
         arr[1] = 1;
@@ -32,4 +40,19 @@ public class Fibonacci {
 
         return arr[n];
     }
+
+    private static long fibNaiveMem(int n, long[] mem) {
+        if (mem[n] != -1)
+            return mem[n];
+
+        if (n <= 1) {
+            return n;
+        }
+
+        long result = fibNaiveMem(n - 1, mem) + fibNaiveMem(n - 2, mem);
+        mem[n] = result;
+
+        return result;
+    }
+
 }
